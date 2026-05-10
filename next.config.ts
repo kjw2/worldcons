dbdfede430e0db0f77bf6374feb3e225edabfff1
kjw2 <jaethis@gmail.com>
2026-05-10 21:31:14 +0900

    Include Undici runtime files in traces

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,7 @@ const nextConfig: NextConfig = {
       "./node_modules/next/dist/server/lib/incremental-cache/shared-cache-controls.external.js",
       "./node_modules/next/dist/server/lib/incremental-cache/tags-manifest.external.js",
       "./node_modules/next/dist/server/response-cache/types.js",
+      "./node_modules/undici/lib/**/*.js",
     ],
   },
   experimental: {
